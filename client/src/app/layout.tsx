4b009adbc8cd037aa223812f28ac79c5973461ea
Yusuf Ozaslan <ozaslanysf@gmail.com>
2024-10-23 20:20:36 +0300

    Init client

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ client/src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import SignIn from "@/features/auth/pages/sign-in";
+
+export default function SignInPage() {
+  return (
+    <SignIn />
+  );
+}
