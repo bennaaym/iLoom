b87039a08f6cfa3867611810f3eb1938e01a82fc
bennaaym <mr.aymen.bennabi@gmail.com>
2024-10-24 16:59:54 +0300

    :adhesive_bandage: Fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,25 +5,17 @@ import {HttpExceptionFilter} from '@common/filters';
 import {ConfigService} from '@modules/config';
 import {RequestResponseInterceptor} from '@common/interceptors';
 import * as session from 'express-session';
-import {Env} from '@common/types';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
 
-  app.use(
-    session({
-      secret: config.sessionSecret,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: config.databaseURI === Env.PRODUCTION,
-        maxAge: 1000 * 60 * 60 * 24
-      }
-    })
-  );
-  app.enableCors();
+  app.enableCors({
+    origin: config.clientAppURL,
+    credentials: true
+  });
+
+  app.use(session(config.session));
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.useGlobalInterceptors(new RequestResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter(config.env));
--- server/src/modules/config/config.service.ts
+++ server/src/modules/config/config.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@nestjs/common';
 import {ConfigSchema} from './config.schema';
 import {ConfigService as NestConfigService} from '@nestjs/config';
+import {Env} from '@common/types';
 
 @Injectable()
 export class ConfigService {
@@ -36,15 +37,27 @@ export class ConfigService {
     } as const;
   }
 
-
   get agora() {
     return {
       appID: this.get('AGORA_APP_ID'),
-      appCertificate: this.get('AGORA_APP_CERTIFICATE'),
+      appCertificate: this.get('AGORA_APP_CERTIFICATE')
     } as const;
   }
 
   get clientAppURL() {
     return this.get('CLIENT_APP_URL');
+  }
+
+  get session() {
+    return {
+      secret: this.sessionSecret,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        httpOnly: this.env === Env.PRODUCTION,
+        secure: this.databaseURI === Env.PRODUCTION,
+        maxAge: 1000 * 60 * 60 * 24
+      }
+    };
   }
 }
